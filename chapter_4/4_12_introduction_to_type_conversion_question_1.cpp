3a8a5bc4c12b83e491bf0973360aed8982e25dd7
VikingOfValhalla <tom.wilson@nordtechsystems.com>
2022-08-31 19:32:11 -0400

    Completed chapter 4.12 quiz.

---
+++ chapter_4/4_12_introduction_to_type_conversion_question_1.cpp
@@ -0,0 +1,18 @@
+#include <cstdint>
+#include <iostream>
+
+char ask_input () {
+    std::cout << "Enter a single character: ";
+    char user_input;
+    std::cin >> user_input;
+    return user_input;
+}
+
+
+int main () {
+    char user_input = ask_input();
+    std::cout << "You entered '" << user_input << "', which has ASCII code " << static_cast<int>(user_input) << '\n';
+
+    return 0;
+}
+
